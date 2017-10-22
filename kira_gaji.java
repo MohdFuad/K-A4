57c0d9e685790be61d99fc10f73de46fa652f74b
MohdFuad <32606896+MohdFuad@users.noreply.github.com>
2017-10-22 15:18:42 +0800

    Add files via upload

---
+++ kira_gaji.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CHEE
+ */
+public class kira_gaji {
+    
+}import java.util.Scanner
+class gaji
+public static void main(String args[])
+System.out.print("Sila masukkan nama");
+System.out.print("Sila masukkan bil_jam_sehari");
+if bil_jam_sehari>9
+System.out.println("Bilangan jam sehari tidak boleh melebihi 9 jam sehari,sila masukkan bilangna yang betul:");
+
+else
+	System.out.print("Sila masukkan bil_hari");
+if bil_hari>30
+System.out.println("Bilangan hari bekerja tidak boleh melebihi 30 hari,masukkan bilangan yang betul");
+
+else
+	System.out.print("Sila masukkan bil_jam_ot");
+if bil_jam_ot>50
+System.out.println("Bilangan jam bekerja lebih masa tidak boleh melebihi 50 jam,masukkan bilangan yang betul");
+
+else 
+	gaji=(bil_jam_sehari*bil_hari*20)+(bil_jam_ot*15)	
+
+System.out.println("Rumusan gaji bulanan"+nama+""
+System.out.println("Jumlah jam bekerja"+bil_jam_sehari+"jam"
+System.out.println("Jumlah hari bekerja:"+bil_hari+"hari"
+System.out.println("Jumlah jam bekerja lebih masa"+bil_jam_ot+"jam"
+System.out.println("Jumlah gaji:RM"+gaji)
+
+
